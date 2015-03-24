1ce8a9b2c243830031bcf196187e014da12ce4b7
Eric Needham <needham_eric@hotmail.com>
2015-03-24 14:45:15 +0000

    assignment

---
+++ config.py
@@ -0,0 +1,3 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'tyke'
+SQLALCHEMY_DATABASE_URI = "postgresql://uwpsqxbjlaxvpc:z-lFkC_0QaPfx_12efrqa9ahiQ@ec2-23-21-231-14.compute-1.amazonaws.com:5432/d3h24ba0ni3gjs"
