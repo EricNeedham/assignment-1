0426f1b4b728da67ac76fcabc058fbe4a77f0bc1
Eric Needham <needham_eric@hotmail.com>
2015-03-26 07:39:47 +0000

    first

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'tyke'
-SQLALCHEMY_DATABASE_URI = "postgresql://uwpsqxbjlaxvpc:z-lFkC_0QaPfx_12efrqa9ahiQ@ec2-23-21-231-14.compute-1.amazonaws.com:5432/d3h24ba0ni3gjs"
+SECRET_KEY = 'tyke21'
+SQLALCHEMY_DATABASE_URI = 'postgresql://action@localhost/action'
